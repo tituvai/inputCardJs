7b082af19724db4047474d92e55cadae134ef450
tituvai <titu17940@gmail.com>
2025-06-16 22:58:50 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// 1st Part 
+let inputValue = document.getElementById("inputValue")
+let inputPage = document.getElementById("inputPage")
+let pustAll = document.getElementById("pustAll")
+let submitBtn = document.getElementById("submitBtn")
+
+// 2nd Part 
+
+let arr = []
+
+
+// 3rd Part 
+function display(){
+    arr.map((item)=>{
+        pustAll.innerHTML += `  <div class="card mt-5" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${item.name}</h5>
+                <p class="card-text">${item.description}</p>
+                <a href="#" class="btn btn-primary">Edit</a>
+                <a href="#" class="btn btn-danger">Delete</a>
+            </div>
+            </div>`
+    })
+}
+
+
+
+// 4th Part 
+
+submitBtn.addEventListener("click", function(){
+    arr.push({
+        name:inputValue.value ,
+        description:inputPage.value
+    })
+    display()
+
+    inputValue.value =""
+    inputPage.value =""
+})
