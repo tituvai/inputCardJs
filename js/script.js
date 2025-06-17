d2e571c76525241455d84961fa07b33614502466
tituvai <titu17940@gmail.com>
2025-06-17 13:39:54 +0600

    addItem

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let inputValue = document.getElementById("inputValue")
 let inputPage = document.getElementById("inputPage")
 let pustAll = document.getElementById("pustAll")
 let submitBtn = document.getElementById("submitBtn")
+let inputError = document.getElementById("inputError")
 
 // 2nd Part 
 
@@ -12,14 +13,14 @@ let arr = []
 // 3rd Part 
 function display(){
     pustAll.innerHTML = ""
-    arr.map((item)=>{
+    arr.map((item, index)=>{
         pustAll.innerHTML += `<div class="col-lg-3">
             <div class="card mt-5" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">${item.name}</h5>
                 <p class="card-text">${item.description}</p>
                 <a href="#" class="btn btn-primary">Edit</a>
-                <a href="#" class="btn btn-danger">Delete</a>
+                <a href="#" class="btn btn-danger" onclick = "deleteItem(${index})">Delete</a>
             </div>
             </div> 
         </div>` 
@@ -31,12 +32,26 @@ function display(){
 // 4th Part 
 
 submitBtn.addEventListener("click", function(){
-    arr.push({
+    if(inputValue.value === "" && inputPage.value === ""){
+        inputError.innerHTML = "places enter your name & description"
+    }
+    else{
+         arr.push({
         name:inputValue.value ,
         description:inputPage.value
     })
     display()
+    }
+   
 
     inputValue.value =""
     inputPage.value =""
 })
+
+
+function deleteItem(index){
+    arr.splice(index, 1)
+    display()
+}
+
+
